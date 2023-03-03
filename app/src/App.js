064a1e0eb3b8d9b90877353529c4fcc359b50cb4
1ikuszak <luke.glica@gmail.com>
2023-03-03 10:37:08 +0100

    changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 import Calculator from './calculator'
+import './App.css'
 
 const App = () => {
+
+
     return (
         <div className='app'>
             <Calculator/>
--- app/src/calculator.jsx
+++ app/src/calculator.jsx
@@ -4,40 +4,40 @@ const Calculator = () => {
     return (
         <div className="calculator">
             <div className="calculator__display">
-                <input></input>
+                <input disabled="true" value="0" maxLength="10"></input>
             </div>
 
             <div className="calculator__buttons">
                 <div className="column">
-                    <button className="calculator__button">C/CE</button>
-                    <button className="calculator__button">MRC</button>
+                    <button className="calculator__button accent">C/CE</button>
+                    <button className="calculator__button accent2">MRC</button>
                     <button className="calculator__button">7</button>
                     <button className="calculator__button">4</button>
                     <button className="calculator__button">1</button>
                     <button className="calculator__button">0</button>
                 </div>
                 <div className="column">
-                    <button className="calculator__button">OFF</button>
-                    <button className="calculator__button">M-</button>
+                    <button className="calculator__button accent">OFF</button>
+                    <button className="calculator__button accent2">M-</button>
                     <button className="calculator__button">8</button>
                     <button className="calculator__button">5</button>
                     <button className="calculator__button">2</button>
                     <button className="calculator__button">.</button>
                 </div>
                 <div className="column">
-                    <button className="calculator__button">√</button>
-                    <button className="calculator__button">M+</button>
+                    <button className="calculator__button accent">√</button>
+                    <button className="calculator__button accent2">M+</button>
                     <button className="calculator__button">9</button>
                     <button className="calculator__button">6</button>
                     <button className="calculator__button">3</button>
-                    <button className="calculator__button">=</button>
+                    <button className="calculator__button accent">=</button>
                 </div>
                 <div className="column">
-                    <button className="calculator__button">%</button>
-                    <button className="calculator__button">÷</button>
-                    <button className="calculator__button">x</button>
-                    <button className="calculator__button">-</button>
-                    <button className="calculator__button">+</button>
+                    <button className="calculator__button accent">%</button>
+                    <button className="calculator__button accent">÷</button>
+                    <button className="calculator__button accent">x</button>
+                    <button className="calculator__button accent">-</button>
+                    <button className="calculator__button add-button accent">+</button>
                 </div>
             </div>
         
